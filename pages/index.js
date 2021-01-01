a3ec02eeb999418270eabc8e9693c8051d6629b5
Motyar <dharmmotyar@gmail.com>
2021-01-01 13:31:16 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -90,21 +90,21 @@ export default function Index({ config, kvMonitors, kvMonitorsLastUpdate }) {
         })}
         <div className="flex flex-row justify-between mt-4 text-sm">
           <div>
-            Powered by{' '}
-            <a href="https://workers.cloudflare.com/" target="_blank">
-              Cloudflare Workers{' '}
+            
+            <a href="https://bruzu.com" target="_blank">
+              Home
             </a>
             &{' '}
-            <a href="https://flareact.com/" target="_blank">
-              Flareact{' '}
+            <a href="https://bruzu.com/" target="_blank">
+              .
             </a>
           </div>
           <div>
             <a
-              href="https://github.com/eidam/cf-workers-status-page"
+              href="https://bruzu.com"
               target="_blank"
             >
-              Get Your Status Page
+              Bruzu.com
             </a>
           </div>
         </div>
